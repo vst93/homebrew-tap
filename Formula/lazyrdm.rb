3bbc40caaaa502922e0c1ede073a57d2118c7a4a
vst <vst93g@gmail.com>
2025-12-04 19:15:19 +0800

    refactor: 更新lazyrdm的下载链接和sha256值，增加对Linux ARM架构的支持

--- Formula/lazyrdm.rb
+++ Formula/lazyrdm.rb
@@ -2,20 +2,32 @@ class Lazyrdm < Formula
   desc "Redis management tool designed for the terminal"
   homepage "https://github.com/vst93/lazyrdm"
   version "1.1"
+  url ""
+  sha256 ""
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/vst93/lazyrdm/releases/download/#{version}/lazyrdm_#{version}_macos_arm64.zip"
-      sha256 "64497171c7b0dda3fa48225ac62039d149d9c20ce17cee88619e01a78027de32"
+      url homepage + "/releases/download/#{version}/lazyrdm-darwin-arm64.zip"
+      sha256 "1d726ce214fad246a3911ed3f9c98988a66df1610d373a633659da7f1551d3a3"
     else
-      url "https://github.com/vst93/lazyrdm/releases/download/#{version}/lazyrdm_#{version}_macos_amd64.zip"
-      sha256 "360d1006beabd7418c54c8029c28f9aaf3f7348d1bb1de2ad5d2d54d7a613f04"
+      url homepage + "/releases/download/#{version}/lazyrdm-darwin-amd64.zip"
+      sha256 "3208667d66aadfd560fa2d9b6171d266d0c5e5de69d2e9556aabea5cfd62c74f"
     end
   end
 
   if OS.linux?
-    url "https://github.com/vst93/lazyrdm/releases/download/#{version}/lazyrdm_#{version}_linux_amd64.zip"
-    sha256 "62665ef6d01b482082264c46ca7328a6440d1e640ad9d5b8b50f86e0817a267c"
+    if Hardware::CPU.arm?
+      url homepage + "/releases/download/#{version}/lazyrdm-linux-arm64.zip"
+      sha256 "902a0b784d2746f4fa818afed42fbc0e86aa5cb19aee2ff95401c1aa763493ae"
+    else
+      url homepage + "/releases/download/#{version}/lazyrdm-linux-amd64.zip"
+      sha256 "e795778242c04e3554e6a0f35ca934d507b294663f1af119604ec15fd35385a5"
+    end
+  end
+
+  if url == ""
+    onoe "No release found for your platform"
+    exit 1
   end
 
   def install
