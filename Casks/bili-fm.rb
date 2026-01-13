eba152045d363f5a4735489581ed52b0366826c0
vst <vst93g@gmail.com>
2026-01-13 14:21:17 +0800

    fix: 更新bili-fm版本到1.4.2并修正sha256值

--- Casks/bili-fm.rb
+++ Casks/bili-fm.rb
@@ -1,9 +1,9 @@
 cask "bili-fm" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.4.1"
-  sha256 arm:   "9ace074f8b88289d24d7278688fa9671ce035da5a5dd7714cdae0fb67641b34e",
-         intel: "5ff8f8cab8158d8ac7cde15b2ac31e4c02be43d82260674eab6ea01567639837"
+  version "1.4.2"
+  sha256 arm:   "61ea7769fe79abb7fcaf3965283d99ad7870815e99cccdae242e7ea9dea5cba7",
+         intel: "70e590bdd3fa2a2392a8ceaa53da67396c5a954fd6dc995d9341a12c7e47706f"
 
   url "https://github.com/vst93/bili-fm/releases/download/#{version}/bili-FM-darwin-#{arch}.zip"
   name "bili-fm"
