a758917099f78d3e67b87e5ce3f3d5af4eb887ba
v <vst93g@gmail.com>
2026-06-02 11:43:59 +0800

    feat: add sfs formula, enable auto-update via sfs Release workflow_run

---
+++ Formula/sfs.rb
@@ -0,0 +1,38 @@
+class Sfs < Formula
+  desc "SmallFileSync - A WebDAV-based terminal file sync tool"
+  homepage "https://github.com/vst93/sfs"
+  version "0.2.0"
+
+  livecheck do
+    url :stable
+    regex(/(\d+\.\d+(\.\d+)?)/i)
+  end
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url homepage + "/releases/download/#{version}/sfs-darwin-arm64.zip"
+      sha256 "0" * 64
+    else
+      url homepage + "/releases/download/#{version}/sfs-darwin-amd64.zip"
+      sha256 "0" * 64
+    end
+  end
+
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url homepage + "/releases/download/#{version}/sfs-linux-arm64.zip"
+      sha256 "0" * 64
+    else
+      url homepage + "/releases/download/#{version}/sfs-linux-amd64.zip"
+      sha256 "0" * 64
+    end
+  end
+
+  def install
+    bin.install "sfs"
+  end
+
+  test do
+    system "#{bin}/sfs", "--version"
+  end
+end
